5f254d05a532b44dd006b44ddde60ea92391cc6b
Philippe Pittoli <kane.root@gmail.com>
2011-03-26 11:22:58 +0100

    Ajout du projet

---
+++ src/abo_sncf/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package abo_sncf;
+
+/**
+ *
+ * @author kane
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
